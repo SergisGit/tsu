ea93eba3a823ff0c0483ae8f348dd063b025ce38
Sergis <si@tagree.ru>
2018-11-21 00:27:04 +0700

    u

--- scripts/main.js
+++ scripts/main.js
@@ -22,6 +22,9 @@
 
   var jsMap = document.querySelector('#js-map');
   if (jsMap) {
+
+    var graduatesData = jsMap.getAttribute('data-graduates');
+
     var map = new mapboxgl.Map({
       container: jsMap,
       style: 'mapbox://styles/isserg/cjomx48pj92fk2st9b0712bk1',
@@ -29,8 +32,99 @@
       zoom: mapZoom,
       minZoom: mapZoom,
       maxZoom: mapZoom,
-      scrollZoom: false
+      scrollZoom: false,
+      dragRotate: false,
+      touchZoomRotate: false
     });
+
+    $.getJSON(graduatesData, function (graduates) {
+
+      map.on('load', function() {
+
+        map.addSource('graduates', {
+          type: 'geojson',
+          data: graduates
+        });
+
+        var grPopup = new mapboxgl.Popup({
+          closeButton: true,
+          closeOnClick: true,
+          className: 'map__popup',
+          offset: 25
+        });
+
+        var activeMarker;
+
+        grPopup.on('close',function() {
+          if (activeMarker) {
+            activeMarker.classList.remove('map__marker--active');
+          }
+        });
+
+        var iconTemplate = 'data:image/svg+xml;utf8,<svg version=\'1.0\' viewBox=\'0 0 24 24\' xml:space=\'preserve\' xmlns=\'http://www.w3.org/2000/svg\' xmlns:xlink=\'http://www.w3.org/1999/xlink\'><g fill=\'%230047FF\'><polygon points=\'12,3 1,9 12,15 23,9 \'/><polygon points=\'19,12.8 12,17 5,12.8 5,17.2 12,21 19,17.2 \'/><rect height=\'8\' width=\'2\' x=\'21\' y=\'9\'/></g></svg>';
+
+        graduates.features.forEach(function(marker, i) {
+
+          var icon = marker.properties.icon ? marker.properties.icon : iconTemplate;
+
+          var el = document.createElement('div');
+          el.id = "marker-" + i;
+          el.className = 'map__marker';
+          el.innerHTML = '<img src="'+icon+'" alt="graduate" class="map__marker-icon">';
+  
+          new mapboxgl.Marker(el, {offset: [0, 0]})
+              .setLngLat(marker.geometry.coordinates)
+              .addTo(map);
+      
+          el.addEventListener('click', function(e){
+            e.stopPropagation();
+  
+            if (activeMarker) {
+              activeMarker.classList.remove('map__marker--active');
+            }
+            activeMarker = this;
+            this.classList.add('map__marker--active');
+
+            var name = marker.properties.name ? marker.properties.name : 'Неизвестный';
+            var imageSrc = marker.properties.image ? marker.properties.image : iconTemplate;
+            var year = marker.properties.year ? '<div class="infowindow__year">’'+marker.properties.year+'</div>' : '';
+            var place = marker.properties.place ? '<div class="infowindow__place">'+marker.properties.place+'</div>' : '';
+            var link = marker.properties.link ? '<a href="'+marker.properties.link+'" class="infowindow__btn  btn">ЧИТАТЬ ИСТОРИЮ</a>' : '';
+
+            grPopup.setHTML(
+              '<div class="infowindow">'+
+                '<div class="infowindow__image-wrapper">'+
+                  '<img class="infowindow__image" src="'+imageSrc+'" alt="'+name+'">'+year+
+                '</div>'+
+                '<div class="infowindow__content">'+
+                  '<div class="infowindow__name">'+name+'</div>'+place+link+
+                '</div>'+
+              '</div>'
+            );
+            grPopup.setLngLat({"lng": marker.geometry.coordinates[0], "lat": marker.geometry.coordinates[1]}).addTo(map);
+          });
+        });
+
+        /*
+        map.on('dragstart', function (event) {
+          console.log(map.tap);
+          if (event.originalEvent && 'touches' in event.originalEvent) {
+            if (event.originalEvent.touches.length < 2) {
+              map.dragPan.disable();
+              map.dragPan.enable();
+            }
+          }
+        });
+        */
+      });
+
+
+
+    });
+
+    
+
+    
   }
   
 })();
